d0567cf9094395e32879bdbe2801b8c2522ff67f
5Prabesh <pbakhrel5@gmail.com>
2024-12-21 12:05:35 +0545

    starter files

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import OnBoardingScreen from './AuthScreens/OnBoardingScreen';
+
+const App = () => {
+  return (
+    <OnBoardingScreen/>
+  )
+}
+
+export default App;
